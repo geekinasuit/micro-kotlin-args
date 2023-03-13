9a926bb452a199504b3e053bb180ab953dac999c
Christian Gruber <christian@geekinasuit.com>
2023-03-13 12:49:16 -0700

    Make a local include example.

--- examples/kscript_local_example.kts
+++ examples/kscript_local_example.kts
@@ -1,15 +1,15 @@
 #!/usr/bin/env kscript
 
-@file:Include("https://raw.githubusercontent.com/geekinasuit/micro-kotlin-args/v0.0.1/src/main/kotlin/ArgsParser.kt")
+@file:Include("../src/main/kotlin/ArgsParser.kt")
 
 import java.lang.RuntimeException
 import kotlin.system.exitProcess
 
 class CLI(val args: ArgsParser) {
-  val foo by args.flag("--foo", help = "It's a foo, whaddya want?!")
+  val foo by args.flag("--foo", help = "It's a foo, whaddya want?!") { toInt() }
+
   val bar by args.opt("-b", help = "expecting lots of that B.")
 }
-
 
 val cli = CLI(ArgsParser(*args, binaryName = "basic.kts"))
 try {
@@ -17,7 +17,7 @@ try {
     println(cli.args.help())
     exitProcess(0)
   }
-  println("FOO: ${cli.foo}")
+  println("FOO: ${cli.foo * 2}")
   println("BAR: ${cli.bar}")
 
 } catch (e: RuntimeException) {
---
+++ examples/kscript_remote_example.kts
@@ -0,0 +1,26 @@
+#!/usr/bin/env kscript
+
+@file:Include("https://raw.githubusercontent.com/geekinasuit/micro-kotlin-args/v0.0.1/src/main/kotlin/ArgsParser.kt")
+
+import java.lang.RuntimeException
+import kotlin.system.exitProcess
+
+class CLI(val args: ArgsParser) {
+  val foo by args.flag("--foo", help = "It's a foo, whaddya want?!")
+  val bar by args.opt("-b", help = "expecting lots of that B.")
+}
+
+val cli = CLI(ArgsParser(*args, binaryName = "basic.kts"))
+try {
+  if ("--help" in args) {
+    println(cli.args.help())
+    exitProcess(0)
+  }
+  println("FOO: ${cli.foo}")
+  println("BAR: ${cli.bar}")
+
+} catch (e: RuntimeException) {
+  println("${e.message}")
+  println(cli.args.help())
+  exitProcess(0)
+}
